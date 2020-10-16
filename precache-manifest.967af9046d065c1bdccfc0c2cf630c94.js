0d06d874cae185e8453798ebd0499c586f957c33
Charlie <cennis92@live.com>
2020-10-16 09:05:45 -0600

    Updates

---
+++ precache-manifest.967af9046d065c1bdccfc0c2cf630c94.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98ad8e6fa82724633e4dad8ced9145f0",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "ae2c0737ecba928b7585",
+    "url": "/React-Portfolio/static/css/2.70e35f80.chunk.css"
+  },
+  {
+    "revision": "4f2c59d225996a0f40e2",
+    "url": "/React-Portfolio/static/css/main.1aa5af5f.chunk.css"
+  },
+  {
+    "revision": "ae2c0737ecba928b7585",
+    "url": "/React-Portfolio/static/js/2.0598f8d3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-Portfolio/static/js/2.0598f8d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f2c59d225996a0f40e2",
+    "url": "/React-Portfolio/static/js/main.0e394c5a.chunk.js"
+  },
+  {
+    "revision": "9c0d314bd2054f86cae4",
+    "url": "/React-Portfolio/static/js/runtime-main.f60b3935.js"
+  },
+  {
+    "revision": "4a7290fca21fbf8482f27dc2c766c804",
+    "url": "/React-Portfolio/static/media/Front End Development.4a7290fc.jpg"
+  },
+  {
+    "revision": "30b9771e819f8f2023feec3fc12b28de",
+    "url": "/React-Portfolio/static/media/Missoula.30b9771e.jpg"
+  },
+  {
+    "revision": "6e974bf4fc6ab27b532ff300a1926c05",
+    "url": "/React-Portfolio/static/media/Travel.6e974bf4.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
